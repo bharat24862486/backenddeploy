15e9eb0cc19cc908e1ff1e2d6223051559b994e1
Bhusham <bhushan@gmail.com>
2023-05-02 15:36:24 +0530

    check

--- route/post.route.js
+++ route/post.route.js
@@ -3,36 +3,62 @@ const postModel = require("../module/post.model")
 
 const postRoute = express()
 
-postRoute.get("/", async(req,res)=>{
+postRoute.get("/", async (req, res) => {
+    let { device, device1, devide2 } = req.query
+
+
     try {
-        let data = await postModel.find()
-        res.send(data)
+        
+        if (device) {
+            let data = await postModel.find({ device: device })
+            res.send(data)
+        } else if(device1&&devide2) {
+            let data = await postModel.find({ $or: [{ device: device1 }, { device: devide2 }] })
+            res.send(data)
+        } else{
+            let data = await postModel.find()
+            res.send(data)
+        }
+
     } catch (error) {
-        res.send({"err": error})
+        res.send({ "err": error })
     }
 })
 
-postRoute.post("/addpost", async(req,res)=>{
+postRoute.post("/addpost", async (req, res) => {
     let body = req.body
     console.log(body)
     try {
         let data = new postModel(body)
         await data.save()
-        res.send({"msg":"data added successfully"})
+        res.send({ "msg": "data added successfully" })
     } catch (error) {
-        res.send({"err": error})
+        res.send({ "err": error })
     }
 })
 
 
-postRoute.delete("/delete/:id", async(req,res)=>{
-    let {id} = req.params
+postRoute.patch("/update/:id", async (req, res) => {
+    let { id } = req.params
+    let body = req.body
 
     try {
-        let data = await postModel.findByIdAndDelete({_id:id})
-        res.send({"msg":"post deleted successfully"})
+        let data = await postModel.findByIdAndUpdate({ _id: id }, body)
+        res.send({ "msg": "post updated successfully" })
     } catch (error) {
-        res.send({'err': error})
+        res.send({ 'err': error })
+    }
+})
+
+
+postRoute.delete("/delete/:id", async (req, res) => {
+    let { id } = req.params
+
+    try {
+        let data = await postModel.findByIdAndDelete({ _id: id })
+        res.send({ "msg": "post deleted successfully" })
+    } catch (error) {
+        res.send({ 'err': error })
     }
 })
 
